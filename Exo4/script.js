890c4d630e07d401881b3fb6bd79b8aa734d665d
Frederic Branca <frederic.branca@gmail.com>
2023-04-12 11:53:33 +0200

    add random color

--- Exo4/script.js
+++ Exo4/script.js
@@ -3,25 +3,22 @@ const block = document.createElement("div") // créer une div
 block.classList.add("block"); //class="block"
 const board = document.querySelector("#board")
 
-// const randomColor = () => {
-//     let rColor = (Math.floor(Math.random() *0xFFFFFF)).toString(16);
-//     document.div.style.backgroundColor = "#" + rColor;
-// }
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
 
 //méthode
 
 
 document.addEventListener('keydown', (event)=> {  
         console.log(event); // all event related info
-        const newBlock = block.cloneNode(); // permet de cloner la div block
 });
 
 document.addEventListener('keydown', (event)=> {  
     if (event.key === "ArrowDown") {
         const newBlock = block.cloneNode(); // permet de cloner la div block
+        newBlock.style.backgroundColor = "rgb(" + getRandomInt(256) + " ," + getRandomInt(256) + " ," + getRandomInt(256) + ")";
+        console.log(newBlock.style.backgroundColor);
         board.appendChild(newBlock)
-        newBlock.classList.add("block")
-        newBlock.classList.
     }
-    
-});
+})
