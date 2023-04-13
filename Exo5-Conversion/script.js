8e120c4da7ad8f413254e1e5467db6cbc1d670e9
Frederic Branca <frederic.branca@gmail.com>
2023-04-13 09:54:13 +0200

    ajout de commentaires

--- Exo5-Conversion/script.js
+++ Exo5-Conversion/script.js
@@ -1,14 +1,26 @@
+//J'ai mit des élements inutiles (pour m'entraîner)
+
+//Constante pour simplifier l'écriture et que ce soit plus lisible
 const section = document.querySelector("#page");
 const conversion = section.querySelector(".conversion");
+
+//Création élément <h1> pour le titre
 var titre = document.createElement("h1");
 titre.innerHTML = "Valeur en euros";
+//Ajoute le titre à la div conversion
 conversion.appendChild(titre);
+
+//Création élement <div> avec id="input" puis ajoute la div dans conversion
 var div = document.createElement("div");
 div.id = "input";
 conversion.appendChild(div);
+
+//Création élement <input> avec class="champ" puis l'ajoute à la div créée juste avant
 var form = document.createElement("input");
 form.className = "champ";
 div.appendChild(form);
+
+//Création nouvelle div pour l'affichage de la conversion (sous l'input)
 var div1 = document.createElement("div");
 div1.id = "franc";
 div1.innerHTML = "francs";
